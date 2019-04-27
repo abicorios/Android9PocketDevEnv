2c2ea5778a2020b5ad231ceeaaa2822631a6c825
abicorios <yaroslav.moroz91@gmail.com>
2019-04-27 08:18:13 +0300

    copy from AndroidPocketDevEnv

--- postinst.sh
+++ postinst.sh
@@ -11,3 +11,16 @@ chmod 755 additional.sh
 pacman-key --init
 pacman-key --populate archlinuxarm
 
+pacman -Syyu --noconfirm
+echo 'set -o vi'>>.profile
+pacman -Syu --noconfirm --needed base-devel cmake python python2 clang vim
+echo "if empty(glob('~/.vim/autoload/plug.vim'))
+  silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
+    \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+call plug#begin('~/.vim/plugged')
+Plug 'Valloric/YouCompleteMe', { 'do': './install.py --clang-complete --system-libclang' }
+call plug#end()                                         " >> .vimrc
+vim -c PlugInstall -c qa
+rm -f postinst.sh
