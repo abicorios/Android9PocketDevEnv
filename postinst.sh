e6c7ba3014186489736f57de167bd2c8ec79935e
abicorios <yaroslav.moroz91@gmail.com>
2019-04-27 11:40:54 +0300

    Update postinst.sh

--- postinst.sh
+++ postinst.sh
@@ -2,17 +2,18 @@
 sed -i '$d' .profile
 sed -i s/^S/#S/ /etc/pacman.d/mirrorlist
 sed -i s/^#\ S/S/ /etc/pacman.d/mirrorlist
+sed -i s/populate\ archlinux$/populate\ archlinuxarm/
 
 #Thank AnLinux
 chmod 755 additional.sh
 ./additional.sh
 
 #https://github.com/EXALAB/AnLinux-App/wiki/Arch-Linux--corrupted-packages-with-invalid-gpg-key
-pacman-key --init
-pacman-key --populate archlinuxarm
+#pacman-key --init
+#pacman-key --populate archlinuxarm
 
 echo 'set -o vi'>>.profile
-pacman -Syu --noconfirm --needed base-devel cmake python python2 clang vim git
+pacman -Syyu --noconfirm --needed base-devel cmake python python2 clang vim git
 echo "if empty(glob('~/.vim/autoload/plug.vim'))
   silent !curl -fLo ~/.vim/autoload/plug.vim --create-dirs
     \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
